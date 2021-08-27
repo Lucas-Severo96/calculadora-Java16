e8dedf8ce4a13cee86bed89734b934e38d446bee
Lucas Severo <lucasleite_14.1@hotmail.com>
2021-08-26 21:10:39 -0300

    Projeto Criado

---
+++ src/curso/cod3r/calc/visao/Calculadora.java
@@ -0,0 +1,17 @@
+package curso.cod3r.calc.visao;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame{
+
+	public Calculadora() {
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new Calculadora();
+	}
+}
