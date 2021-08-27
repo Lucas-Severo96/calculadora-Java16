5ab932bd6fa4dea593732a4e49cd8d11a61e5af5
Lucas Severo <lucasleite_14.1@hotmail.com>
2021-08-26 21:33:29 -0300

    Tela Principal - Swing

--- src/curso/cod3r/calc/visao/Calculadora.java
+++ src/curso/cod3r/calc/visao/Calculadora.java
@@ -7,7 +7,9 @@ public class Calculadora extends JFrame{
 
 	public Calculadora() {
 		
+		setSize(232, 322);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 	
